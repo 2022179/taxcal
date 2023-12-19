79b834c6a16996760b55a62629dcabc977cfdee2
2022503 <146199452+2022503@users.noreply.github.com>
2023-12-19 23:56:46 +0000

    updated the DBWriter
    updated the DBWriter

--- src/taxcal/DBWriter.java
+++ src/taxcal/DBWriter.java
@@ -4,10 +4,38 @@
  */
 package taxcal;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 /**
  *
  * @author User
  */
-public class DBWriter {
+public class DBWriter extends Database {
+    public boolean addUser(User user) {
+     try(
+          Connection conn = DriverManager.getConnection(DB_URL, USER,PASSWORD);   
+          Statement stmt = conn.createStatement();
+             
+        ){
+     String sql = String.format("INSERT INTO" + TABLE_NAME +" VALUES ("+"'%S', '%S', '%S', '%d');"
+     
+     user.getName(),)
+     
+     
+     
+     }catch(Exception e){
+     
+     
+     
+     }
+     
+     
+     
+     
+    }
+    
+    
     
 }
