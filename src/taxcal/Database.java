c4c671b112363879bd8ed9be2969a253c6610155
2022503 <146199452+2022503@users.noreply.github.com>
2023-12-19 23:15:07 +0000

    created a Database class and added few lines of code
    created a Database class and added a few lines of code

--- src/taxcal/Database.java
+++ src/taxcal/Database.java
@@ -9,5 +9,14 @@ package taxcal;
  * @author User
  */
 public class Database {
+    proctected final static String DB_BASE_URL = "jdbc:mysql://localhost";
+    //jdbc=java is a database connect
+    protected final static String USER= "ooc2023";
+    protected final static String PASSWORD ="ooc2023";
+    // this will shows the database connected
+    protected final static String DABE_NAME = "taxcal";
+    protected final static String TABLE_NAME = "userdata";
+    //jdbc:mysql://localhost/taxcal
+    protected final static String DB_URL = DB_BASE_URL +"/"+ DB_NAME;
     
 }
