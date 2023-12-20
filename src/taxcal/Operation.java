0141fe10196f1228a21c430e3e299d0ee2f7d1ab
2022503 <146199452+2022503@users.noreply.github.com>
2023-12-20 15:43:41 +0000

    Created ennum operation class
    Created ennum operation class

---
+++ src/taxcal/Operation.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package taxcal;
+
+/**
+ *
+ * @author User
+ */
+public enum Operation {
+    CREATE_USER,
+    MODIFY_USER,
+    REMOVE_USER,
+    VIEW_USER,
+    CALCULATE_INCOME_TAX  
+}
